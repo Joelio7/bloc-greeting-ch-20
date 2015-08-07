c497ebedd2de4db4eef2fe12f044ab72ca221851
Your Name <joe.scalera@gmail.com>
2015-08-07 02:23:44 -0700

    bloc-greeting-ch-20 git commit -m bloc-greeting-ch-20 git status ls git commit -m bloc-greeting-ch-20

---
+++ greeting.rb
@@ -0,0 +1,18 @@
+def hey
+  array = ["Sterling","Cheryl","Lana"]
+  array.each do |item|
+  puts  "hey #{item}"
+
+  end
+
+end
+  print hey
+
+
+def yo
+  array = ["Cyril","Archer","Krieger"]
+  array.each do |item|
+    puts "yo #{item}"
+  end
+end
+print yo
